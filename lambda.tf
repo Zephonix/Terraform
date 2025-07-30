514acc93784003a73b00358d848d31a34ebb1e97
Jonathan Ray Cardenas <jcardenas@partnerone.com>
2025-07-30 17:10:20 -0500

    change space

--- lambda.tf
+++ lambda.tf
@@ -4,13 +4,11 @@
 --------------*/
 
 resource "aws_lambda_function" "test_lambda" {
-
   runtime       = "python3.9"
   filename      = "code.zip"
   function_name = "lambaTerraform"
   handler       = "code.lambda_handler"
   role          = aws_iam_role.S3PutObjectRoleTf.arn
-
 }
 
 output "arn_lambda" {
